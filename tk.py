7431becb5aa21b4ce802da5a63eea068f1204f4c
ayushi1376 <51190203+ayushi1376@users.noreply.github.com>
2019-05-30 21:03:37 +0530

    Update tk.py

--- tk.py
+++ tk.py
@@ -1,27 +1,4 @@
-import tkinter
-from tkinter import *
-
-mainWindow = tkinter.Tk()
-mainWindow.title("Example Tkinter")
-
-headingLabel = tkinter.Label(mainWindow, text="Hello World")
-headingLabel.pack()
-
-userEntry = Entry(mainWindow)
-userEntry.pack()
-
-def fun():
-    # print("Hello")
-    name = userEntry.get()
-    print("Hello your name is: ", name)
-
-showButton = tkinter.Button(mainWindow, text="Button", command=fun())
-showButton.pack()
-
-mainWindow.mainloop()
-
-
-## Simple Calculator
+## Simple Calculator with exception handling
 
 
 import tkinter as tk
